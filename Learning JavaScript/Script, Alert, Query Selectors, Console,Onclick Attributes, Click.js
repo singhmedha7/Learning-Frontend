96982b781ea1cb09a53efa57c05e27585c6aa32d
singhmedha7 <singhmedha2007@gmail.com>
2026-07-12 21:15:27 +0530

    JS file

---
+++ Learning JavaScript/Script, Alert, Query Selectors, Console,Onclick Attributes, Click.js
@@ -0,0 +1,7 @@
+console.log('Index.js was added')
+ alert('I am inside html page learning JS');
+console.log('Hello');
+console.log(2+2);
+console.log('good' + 'morning');
+console.warn('Warning');
+console.error('This is an error');
