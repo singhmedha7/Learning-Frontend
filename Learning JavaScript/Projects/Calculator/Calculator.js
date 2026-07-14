7266d2d31e85e2fd9f8237c52f7f23f7e58dffb9
singhmedha7 <singhmedha2007@gmail.com>
2026-07-14 20:47:58 +0530

    Resiziing

--- Learning JavaScript/Projects/Calculator/Calculator.js
+++ Learning JavaScript/Projects/Calculator/Calculator.js
@@ -1,2 +1,11 @@
+// Automatically resizes the window when the app opens
+window.addEventListener('DOMContentLoaded', () => {
+    // Set your target width and height in pixels
+    const appWidth = 277; 
+    const appHeight = 520; 
+
+    window.resizeTo(appWidth, appHeight);
+});
+
 let currentDisplay = '';
 document.querySelector('#display').value = currentDisplay;
