613cdcbcf55281a4ef554c852c70d807b43d60a3
singhmedha7 <singhmedha2007@gmail.com>
2026-07-26 20:50:57 +0530

    To Do List

---
+++ Learning JavaScript/Projects/To Do App/To Do.js
@@ -0,0 +1,42 @@
+let todoList=JSON.parse(localStorage.getItem('todoList'))||[
+    {
+        item:'', 
+        dueDate: ''
+    },
+    
+];
+
+displayItems();
+
+
+function addTask(){
+    let inputElement=document.querySelector('#todo-input');
+    let dateElement=document.querySelector('#todo-date');
+    let todoItem=inputElement.value;
+    let todoDate=dateElement.value;
+    todoList.push({item: todoItem, dueDate:todoDate});
+    inputElement.value='';
+    dateElement.value='';
+    displayItems();
+}
+
+
+function displayItems(){
+    let containerElement=document.querySelector('.todo-container');
+
+    let newHTML='';
+
+    for(let i=0;i<todoList.length;i++){
+        let {item}=todoList[i];
+        let {dueDate}=todoList[i];
+         newHTML+=`
+        
+        <span>${item}</span>
+        <span>${dueDate}</span>
+        <button class="btn-delete" onclick="todoList.splice(${i},1);
+        displayItems()">Delete</button>
+        `;
+        }
+        containerElement.innerHTML = newHTML;
+        localStorage.setItem('todoList',JSON.stringify(todoList));
+}
